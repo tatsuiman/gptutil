2f933550eb1a820304789c3a31f85599d6eb3410
tatsui <2360691+tatsu-i@users.noreply.github.com>
2023-03-15 12:01:08 +0000

    first commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gpt-cli",
-    version="0.1.0",
+    name="gpt-tools",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "click",
